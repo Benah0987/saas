af4e8568536ca5d00ce7e9a84b7458460e250e0a
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2025-05-10 09:04:29 +0300

    send code

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,7 @@
-import mongoose from 'mongoose';
-
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model('User', userSchema);
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false } // 👈 Optional, defaults to false
+  }, { timestamps: true });
+  
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -208,3 +208,5 @@ const Profile = () => {
 };
 
 export default Profile;
+
+
